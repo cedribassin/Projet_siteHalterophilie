a4a0286ca87903e3ba7dbb7f6de0b5b8a05910ab
Cédric R <cedric.ribassin@gmail.com>
2020-11-12 10:37:27 -0500

    Possibilité de modifier et supprimer des mouvements, et mise en place de la partie admin pour les types de mouvements (ajout, modification et suppression des types de mouvement)

--- src/Controller/Admin/AdminMouvementController.php
+++ src/Controller/Admin/AdminMouvementController.php
@@ -12,6 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminMouvementController extends AbstractController
@@ -28,16 +29,19 @@ class AdminMouvementController extends AbstractController
     }
 
     /**
-     * @Route("/admin/mouvement/creation", name="admin_creation")
+     * @Route("/admin/mouvement/creation", name="admin_mouvement_creation")
+     * @Route("/admin/mouvement/{id}", name="admin_modification_mouvement", methods="GET|POST")
      */
-    public function ajoutModification(Mouvement $mouvement = null, Request $request, EntityManagerInterface $entityManager)
+    public function ajoutModificationMouvements(Mouvement $mouvement = null, Request $request, EntityManagerInterface $entityManager)
     {
-        $mouvement = new Mouvement();
+        if(!$mouvement){
+            $mouvement = new Mouvement();
+        }
         $form = $this->createFormBuilder($mouvement)
             ->add('nom', TextType::class)
             ->add('objectif', TextType::class)
             ->add('intensite', IntegerType::class)
-            ->add('consignes', TextType::class)
+            ->add('consignes', TextareaType::class)
             ->add('erreur', TextType::class)
             ->add('correctif', TextType::class)
             ->add('image', TextType::class)
@@ -50,8 +54,10 @@ class AdminMouvementController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
+            $modif = $mouvement->getId() !== null; 
             $entityManager->persist($mouvement);
             $entityManager->flush();
+            $this->addFlash("success", $modif ? "La modification a été effectuée" : "L'ajout a été effectuée");
             return $this->redirectToRoute("admin_mouvements");
         }
         return $this->render('admin/admin_mouvement/ajoutModifMouvement.html.twig', [
@@ -60,6 +66,23 @@ class AdminMouvementController extends AbstractController
         ]);
     }
 
+     /**
+     * @Route("/admin/mouvement/{id}", name="admin_suppression_mouvement", methods="delete")
+     */
+    //Fonction permettant de supprimer un mouvement (en rajoutant methods="delete" dans les routes, on va pouvoir distinguer
+    // son url de celle présente dans la fonction ajoutEtModification())
+    public function suppressionMouvement(Mouvement $mouvement, Request $request, EntityManagerInterface $entityManager)
+    {
+        //On vérifie que le token de la requete est valide en vérifiant si les info sont identiques à celles pour générer 
+        // le token dans la vue adminMouvement.html.twig.php, et si c'est la cas alors on peut réaliser les actions en BDD
+        if ($this->isCsrfTokenValid("SUP". $mouvement->getId(), $request->get('_token'))){
+        //On prépare la requête de suppression avec remove() et on envoie en BDD avec flush()
+        $entityManager->remove($mouvement);
+        $entityManager->flush();
+        $this->addFlash("success","La suppression a été effectuée");
+        return $this->redirectToRoute("admin_mouvements");
+        }
+    }
 
 
 }
---
+++ src/Controller/Admin/AdminTypeMouvementController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Mouvement;
+use App\Entity\TypeMouvement;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\TypeMouvementRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminTypeMouvementController extends AbstractController
+{
+    /**
+     * @Route("/admin/types", name="admin_types")
+     */
+    public function index(TypeMouvementRepository $repository)
+    {
+        $typeMouvements=$repository->findAll();
+        return $this->render('admin/admin_type_mouvement/adminTypeMouvement.html.twig', [
+            'typeMvts' => $typeMouvements
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/type/creation", name="admin_typeMouvement_creation")
+     * @Route("/admin/type/{id}", name="admin_modification_typeMouvement", methods="GET|POST")
+     */
+    public function ajoutModificationTypes(TypeMouvement $typeMouvement = null, Request $request, EntityManagerInterface $entityManager)
+    {
+        if(!$typeMouvement){
+            $typeMouvement = new TypeMouvement();
+        }
+
+        $form = $this->createFormBuilder($typeMouvement)
+            ->add('libelle', TextType::class)
+            ->add('image', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('mouvement', EntityType::class, [ 
+                'class' => Mouvement::class, 
+                'choice_label' => 'nom',
+                'expanded'=>true,
+                'multiple'=>true ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $modif = $typeMouvement->getId() !== null; 
+            $entityManager->persist($typeMouvement);
+            $entityManager->flush();
+            $this->addFlash("success", $modif ? "La modification a été effectuée" : "L'ajout a été effectuée");
+            return $this->redirectToRoute("admin_types");
+        }
+
+        return $this->render('admin/admin_type_mouvement/ajoutModifTypeMouvement.html.twig',[
+            'typeMvt'=>$typeMouvement,
+            'form'=>$form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/type/{id}", name="admin_suppression_typeMouvement", methods="delete")
+     */
+    public function suppressionMouvement(TypeMouvement $typeMouvement, Request $request, EntityManagerInterface $entityManager)
+    {
+        //On vérifie que le token de la requete est valide en vérifiant si les info sont identiques à celles pour générer 
+        // le token dans la vue adminMouvement.html.twig.php, et si c'est la cas alors on peut réaliser les actions en BDD
+        if ($this->isCsrfTokenValid("SUP". $typeMouvement->getId(), $request->get('_token'))){
+        //On prépare la requête de suppression avec remove() et on envoie en BDD avec flush()
+        $entityManager->remove($typeMouvement);
+        $entityManager->flush();
+        $this->addFlash("success","La suppression a été effectuée");
+        return $this->redirectToRoute("admin_types");
+
+        }
+    }
+
+}
