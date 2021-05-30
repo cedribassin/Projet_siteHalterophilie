ef50329bbc0089f6f60b98f4b234dcaaa7780b08
Cédric R <cedric.ribassin@gmail.com>
2021-05-30 13:50:19 -0400

    Mise en place d'une partie utilisateur avec authentification, inscription, connexion et deconnexion

---
+++ src/Controller/UserController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function inscription(Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //On récupère le password et on l'encrypte
+            $passwordCrypte = $encoder->encodePassword($user, $user->getPassword());
+            //On modifie le password avec son setter:
+            $user->setPassword($passwordCrypte);
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute("programme_accueil");
+        }
+        return $this->render('user/inscription.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="connexion")
+     */
+    public function login(AuthenticationUtils $utils)
+    {
+        return $this->render('user/login.html.twig', [
+            "lastUserName" => $utils->getLastUsername(),
+            "error" => $utils->getLastAuthenticationError()
+        ]);
+    }
+
+    /**
+     * @Route("/deconnexion", name="deconnexion")
+     */
+    public function deconnexion()
+    {
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(
+ * fields={"username"},
+ * message="Le user existe déjà")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=3,max=10, minMessage="Le nom d'utilisateur doit être supérieur à 3", maxMessage="Le nom d'utilisateur doit être inférieur à 10")
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=3,max=10, minMessage="Le nombre de caractère doit être supérieur à 3", maxMessage="Le nombre de caractère doit être inférieur à 10")
+     */
+    private $password;
+
+    /**
+     * @Assert\Length(min=3,max=10, minMessage="Le nombre de caractère doit être supérieur à 3", maxMessage="Le nombre de caractère doit être inférieur à 10")
+     * @Assert\EqualTo(propertyPath="password", message="Les mots de passe sont différents")
+     */
+    private $verifPassword;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getVerifPassword(): ?string
+    {
+        return $this->verifPassword;
+    }
+
+    public function setVerifPassword(string $verifPassword): self
+    {
+        $this->verifPassword = $verifPassword;
+
+        return $this;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+
+    public function getSalt()
+    {
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+}
